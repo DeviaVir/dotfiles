026c77786b1c5138b0418c2dd9e9f94da66b0ee5
Chase Sillevis <chase@sillevis.net>
2017-06-16 15:35:26 +0200

    update lockscreen. remove now superfluous gpg-agent.service

---
+++ .scripts/lock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+
+if [[ -f $HOME/.icons/lock.png ]]
+then
+	 convert /tmp/screen.png $HOME/.icons/lock.png -gravity center -composite -matte /tmp/screen.png
+fi
+
+# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png
